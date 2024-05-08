3155f9f317b84ad4e436920b0a7325c7d2474702
Iago Cabral <87732154+iagocabral@users.noreply.github.com>
2024-05-07 22:20:58 -0300

    Create sonar_test.py
    testing sonar

---
+++ sonar_test.py
@@ -0,0 +1,17 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+def main():
+    nums = [64, 25, 12, 22, 11]
+    print("Lista original:", nums)
+    selection_sort(nums)
+    print("Lista ordenada:", nums)
+
+if __name__ == "__main__":
+    main()
