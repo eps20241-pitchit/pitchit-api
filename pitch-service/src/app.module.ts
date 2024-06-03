6842a5151ff6b26b425d42646a921d8c6f1c2222
Douglas Castro <farias.douglascastro@gmail.com>
2024-06-03 02:15:55 -0300

    Adicionado OpenAI module

--- pitch-service/src/app.module.ts
+++ pitch-service/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OpenAIModule } from './openAI/openai.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    envFilePath: '.env',
+    isGlobal: true,
+  }),
+  OpenAIModule],
   controllers: [AppController],
   providers: [AppService],
 })
